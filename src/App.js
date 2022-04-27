224cb0b03356b5240a0afc9f433e67cdafb7f285
louremipsum <myself.vinayak2@gmail.com>
2022-04-27 22:35:03 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Stack, HStack, VStack } from "@chakra-ui/react";
 
 function App() {
   return (
@@ -17,6 +19,20 @@ function App() {
         >
           Learn React
         </a>
+        <Stack direction="row" spacing={4} align="center">
+          <Button colorScheme="teal" variant="solid">
+            Button
+          </Button>
+          <Button colorScheme="teal" variant="outline">
+            Button
+          </Button>
+          <Button colorScheme="teal" variant="ghost">
+            Button
+          </Button>
+          <Button colorScheme="teal" variant="link">
+            Button
+          </Button>
+        </Stack>
       </header>
     </div>
   );
